0f4945fb46b71d801d339bf62d82691b7e00b7b1
Steve Caron <steve.caron@dynatrace.com>
2020-09-14 04:56:31 +0000

    added app URL as argument

--- 2-Basic_Quality_Gate/run_basic_test.sh
+++ 2-Basic_Quality_Gate/run_basic_test.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
-echo 'Gate run #1'
-for i in {1..20} 
-do
-  curl --silent --output nul --show-error 'http://35-192-62-255.nip.io:8070/api/echo?text=Just%20say%20whatever%20you%20want&sleep=500' 
-done
+if [ -z "$1" ]
+then
+  echo "No argument supplied. Please provide the simple node app URL as an argument."
+else
+  # removing potential trailing slash	
+  URL=$(echo "$1" | sed 's/\/*$//g')
 
-sleep 60s
+  echo 'Gate run #1'
+  for i in {1..20} 
+  do
+    curl --silent --output nul --show-error $URL'/api/echo?text=Just%20say%20whatever%20you%20want&sleep=500' 
+  done
 
-keptn send event start-evaluation --project=standalone-qg --stage=qualitystage --service=simplenodeservice --timeframe=2m --labels=gaterun=1,type=viacli
+  sleep 60s
 
-sleep 60s
+  keptn send event start-evaluation --project=standalone-qg --stage=qualitystage --service=simplenodeservice --timeframe=2m --labels=gaterun=1,type=viacli
 
-echo 'Gate run #2'
-for i in {1..20}
-do
-  curl --silent --output nul --show-error 'http://35-192-62-255.nip.io:8070/api/echo?text=Just%20say%20whatever%20you%20want&sleep=800'
-done
+  sleep 60s
 
-sleep 60s
+  echo 'Gate run #2'
+  for i in {1..20}
+  do
+    curl --silent --output nul --show-error $URL'/api/echo?text=Just%20say%20whatever%20you%20want&sleep=800'
+  done
 
-keptn send event start-evaluation --project=standalone-qg --stage=qualitystage --service=simplenodeservice --timeframe=2m --labels=gaterun=2,type=viacli
+  sleep 60s
 
-sleep 60s
+  keptn send event start-evaluation --project=standalone-qg --stage=qualitystage --service=simplenodeservice --timeframe=2m --labels=gaterun=2,type=viacli
 
-echo 'Gate run #3'
-for i in {1..20}
-do
-  curl --silent --output nul --show-error 'http://35-192-62-255.nip.io:8070/api/echo?text=Just%20say%20whatever%20you%20want&sleep=600'
-done
+  sleep 60s
 
-for i in {1..5}
-do
-  curl --silent --output nul --show-error 'http://35-192-62-255.nip.io:8070/api/invoke?url=https://www.amazon.com&sleep=900'
-done
+  echo 'Gate run #3'
+  for i in {1..20}
+  do
+    curl --silent --output nul --show-error $URL'/api/echo?text=Just%20say%20whatever%20you%20want&sleep=600'
+  done
 
-sleep 60s
+  for i in {1..5}
+  do
+    curl --silent --output nul --show-error $URL'/api/invoke?url=https://www.dynatrace.com&sleep=1000'
+  done
 
-keptn send event start-evaluation --project=standalone-qg --stage=qualitystage --service=simplenodeservice --timeframe=2m --labels=gaterun=3,type=viacli
+  sleep 60s
 
+  keptn send event start-evaluation --project=standalone-qg --stage=qualitystage --service=simplenodeservice --timeframe=2m --labels=gaterun=3,type=viacli
+
+  echo DONE!
+fi

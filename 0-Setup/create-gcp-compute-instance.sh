f8adc95acfa6cd8a57c106f83775c5fa47c37fac
steve-caron-dynatrace <steve.caron@dynatrace.com>
2020-09-15 15:55:54 -0400

    added name as a script parameter for labelling

--- 0-Setup/create-gcp-compute-instance.sh
+++ 0-Setup/create-gcp-compute-instance.sh
@@ -8,6 +8,7 @@
 ## You need to provide 2 parameters
 ## $1 : <name of your GCP project> 
 ## $2 : <name of your GCP compute instance>
+## $3 : <your_name_no_nospace>
 
-gcloud beta compute --project=$1 instances create $2 --zone=us-central1-a --machine-type=n2-standard-4 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/devstorage.read_only --tags=http-server,https-server --image=ubuntu-1804-bionic-v20200821a --image-project=ubuntu-os-cloud --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=sc-test --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=owner=stevecaron,purpose=bootcampqg --reservation-affinity=any --metadata startup-script-url=https://raw.githubusercontent.com/steve-caron-dynatrace/se-bootcamp-keptn-qg/master/0-Setup/keptn-in-a-box.sh
+gcloud beta compute --project=$1 instances create $2 --zone=us-central1-a --machine-type=n2-standard-4 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/devstorage.read_only --tags=http-server,https-server --image=ubuntu-1804-bionic-v20200821a --image-project=ubuntu-os-cloud --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=sc-test --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=owner=$3,purpose=bootcampqg --reservation-affinity=any --metadata startup-script-url=https://raw.githubusercontent.com/steve-caron-dynatrace/se-bootcamp-keptn-qg/master/0-Setup/keptn-in-a-box.sh
 

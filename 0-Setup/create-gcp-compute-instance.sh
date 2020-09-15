5f52e94ef2c3dc4936fe63dc594aa8cafb7fc0b1
steve-caron-dynatrace <steve.caron@dynatrace.com>
2020-09-15 15:45:51 -0400

    changed permissions

--- 0-Setup/create-gcp-compute-instance.sh
+++ 0-Setup/create-gcp-compute-instance.sh
@@ -10,3 +10,4 @@
 ## $2 : <name of your GCP compute instance>
 
 gcloud beta compute --project=$1 instances create $2 --zone=us-central1-a --machine-type=n2-standard-4 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/devstorage.read_only --tags=http-server,https-server --image=ubuntu-1804-bionic-v20200821a --image-project=ubuntu-os-cloud --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=sc-test --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=owner=stevecaron,purpose=bootcampqg --reservation-affinity=any --metadata startup-script-url=https://raw.githubusercontent.com/steve-caron-dynatrace/se-bootcamp-keptn-qg/master/0-Setup/keptn-in-a-box.sh
+
